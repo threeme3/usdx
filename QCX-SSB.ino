90ab78845fc0ecfad198a03bea80717bea548b3b
Howard Su <howard0su@gmail.com>
2020-10-09 14:03:39 +0800

    Remove deadcode of original CAT implementation

--- QCX-SSB.ino
+++ QCX-SSB.ino
@@ -3524,24 +3524,6 @@ void print_char(uint8_t in){  // Print char in second line of display and scroll
   cw_event = true;
 }
 
-static char cat[20];  // temporary here
-static uint8_t nc = 0;
-static uint16_t cat_cmd = 0;
-
-void parse_cat(uint8_t in){   // TS480 CAT protocol:  https://www.kenwood.com/i/products/info/amateur/ts_480/pdf/ts_480_pc.pdf
-  if(nc == 0) cat_cmd = in << 8;
-  if(nc == 1) cat_cmd += in;
-  if(in == ';'){  // end of cat command -> parse and process
-
-    switch(cat_cmd){
-      case 'I'<<8|'F': Serial.write("IF00010138000     +00000000002000000 ;"); lcd.setCursor(0, 0); lcd.print("IF"); break;
-    }
-    nc = 0;       // reset
-  } else {
-    if(nc < (sizeof(cat) - 1)) cat[nc++] = in; // buffer and count-up
-  }
-}
-
 void loop()
 {
 #ifdef CAT
