71443a88acf4a7ed2be718d189e0f5267d00fe66
Xu Zhong <john138@gmail.com>
2021-09-21 03:45:46 -0600

    Fix missing 1 voice sample issue

--- QCX-SSB.ino
+++ QCX-SSB.ino
@@ -5168,12 +5168,11 @@ void loop()
   if((vox) && ((mode == LSB) || (mode == USB))){  // If VOX enabled (and in LSB/USB mode), then take mic samples and feed ssb processing function, to derive amplitude, and potentially detect cross vox_threshold to detect a TX or RX event: this is expressed in tx variable
     if(!vox_tx){ // VOX not active
 #ifdef MULTI_ADC
-      if(vox_sample++ == 16){  // take N sample, then process
+      vox_adc += analogSampleMic();
+      if(++vox_sample == 16){  // take N sample, then process
         ssb(((int16_t)(vox_adc/16) - (512 - AF_BIAS)) >> MIC_ATTEN);   // sampling mic
         vox_sample = 0;
         vox_adc = 0;
-      } else {
-        vox_adc += analogSampleMic();
       }
 #else
       ssb(((int16_t)(analogSampleMic()) - 512) >> MIC_ATTEN);   // sampling mic
